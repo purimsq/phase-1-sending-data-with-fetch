bb1ea891635ac4b0095187fddda9e16cb08b8bc4
Dylen Gitaka <dylen.gitaka@student.moringaschool.com>
2024-04-09 05:13:04 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(userName, userEmail){
+    const url = "http://localhost:3000/users";
+  
+    const append = {
+      method : "POST", 
+      headers : {
+        "Content-Type" : "application/json", 
+        "Accept" : "application/json"
+      }, 
+      body : JSON.stringify({
+        name : `${userName}`, 
+        email : `${userEmail}`
+      })
+    };
+  
+    return fetch(url, append)
+      .then(res => res.json())
+      .then(data => document.body.innerHTML = data.id)
+      .catch(error => document.body.innerHTML = error.message)
+  };
+  
+  submitData("Dan Lee", "danlee@gmail.com");
